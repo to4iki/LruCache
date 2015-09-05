3dd411b48c8fa24920ef320c5eed321aa23187eb
to4iki <tsk.take815@gmail.com>
2015-09-05 15:18:08 +0900

    Initial Commit

---
+++ LruCache/LruCache.h
@@ -0,0 +1,19 @@
+//
+//  LruCache.h
+//  LruCache
+//
+//  Created by to4iki on 2015/09/05.
+//  Copyright (c) 2015年 to4iki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LruCache.
+FOUNDATION_EXPORT double LruCacheVersionNumber;
+
+//! Project version string for LruCache.
+FOUNDATION_EXPORT const unsigned char LruCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LruCache/PublicHeader.h>
+
+
